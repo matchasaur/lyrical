d9d221a1b8dfd90e40bbc4d01c30f7d5920e800c
Jared.Tan <Jtanu45@gmail.com>
2024-05-26 20:04:37 -0700

    Audio files upload to supabase

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
+from supabase import create_client, Client
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 app = Flask(__name__)
 cors = CORS(app, origins='*')
@@ -9,10 +17,14 @@ cors = CORS(app, origins='*')
 @app.route('/submit', methods=['POST'])
 def submit():
     file = request.files['file']
-    if file:
-        filename = secure_filename(file.filename)
-        file.save(os.path.join('audios', filename))
-        return 'File uploaded successfully', 200
+    f = file.read()
+    
+    
+    
+    # filename = secure_filename(file.filename)
+    # file.save(os.path.join('audios', filename))
+    supabase.storage.from_("audio").upload(file=f, path=file.filename, file_options={"content-type": "audio/mpeg"})
+    return 'File uploaded successfully', 200
 
 @app.route('/hello', methods=['GET'])
 def hello():
